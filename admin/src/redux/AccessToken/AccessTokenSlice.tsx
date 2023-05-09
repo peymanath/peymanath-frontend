12eedad4bbd876783bd3ee29f167aa6f4c30b76e
peymanath <naderidefault@gmail.com>
2023-05-09 23:54:33 +0430

    added Redux Tollki in admin panel

---
+++ admin/src/redux/AccessToken/AccessTokenSlice.tsx
@@ -0,0 +1,25 @@
+import { AccessTokenState } from "@/types/redux";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState: AccessTokenState = {
+	accessToken: (localStorage.accessToken && JSON.parse(localStorage.accessToken)) || "",
+};
+
+const AccessTokenSlice = createSlice({
+	name: "accessToken",
+	initialState,
+	reducers: {
+		addToken: (state, action: PayloadAction<string>) => {
+			localStorage.setItem(
+				"accessToken",
+				JSON.stringify(action.payload),
+			);
+		
+			state.accessToken = action.payload;
+		},
+	},
+});
+
+export const { addToken } = AccessTokenSlice.actions;
+
+export default AccessTokenSlice.reducer;
