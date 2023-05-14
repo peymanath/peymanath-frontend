93a3cd6251bf24fbbd3d116e1cf691510e4be949
peymanath <naderidefault@gmail.com>
2023-05-14 21:22:01 +0430

    added progress bar

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,29 +1,35 @@
-import "APP/globals.css";
-import type { Metadata } from "next";
+"use client"
+import "STYLE/globals.css";
+import type {Metadata} from "next";
 import Header from "DOMAIN/Header";
 import Footer from "DOMAIN/Footer";
+import Next13NProgress from 'next13-nprogress';
+import type {ReactNode} from "react";
+
 export const metadata: Metadata = {
-	viewport:
-		"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0",
-	icons: "images/favicon.png",
+    viewport:
+        "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0",
+    icons: "images/favicon.png",
 };
 
 export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: ReactNode;
 }) {
-	return (
-		<html lang="fa-IR" dir="rtl">
-			<body className="bg-black text-white">
-				<Header />
+    return (
+        <html lang="fa-IR" dir="rtl">
+        <body className="bg-black text-white">
+        <Header/>
 
-				<div className="fixed w-full h-full inset-0 bg-body bg-center bg-no-repeat opacity-30 -z-[99999]"></div>
+        <Next13NProgress color="var(--primary)" options={{showSpinner: false}}/>
 
-				<main>{children}</main>
+        <div className="fixed w-full h-full inset-0 bg-body bg-center bg-no-repeat opacity-30 -z-[99999]"></div>
 
-				<Footer />
-			</body>
-		</html>
-	);
+        <main>{children}</main>
+
+        <Footer/>
+        </body>
+        </html>
+    );
 }
